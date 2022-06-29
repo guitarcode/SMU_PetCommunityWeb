1977c6af4358a3acff43a77e80c9ab9e8b8aca84
C:\Users\201910086\Desktop\likelion <201910086@sangmyung.kr>
2022-06-29 22:43:46 +0900

    member models.py 중간

--- Django/PetCommunity/account/admin.py
+++ Django/PetCommunity/account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from account.models import Member
 
-# Register your models here.
+admin.site.register(Member, UserAdmin)
+
--- Django/PetCommunity/account/models.py
+++ Django/PetCommunity/account/models.py
@@ -1,4 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.forms import CharField
+from django.core.validators import RegexValidator
 
+class Member(AbstractUser):
+    #회원id : Model에서 자동으로 생성
+    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    # 정규식으로 유효성 검증
+    phoneNumberRegex = RegexValidator(regex = r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
+    phoneNumber = models.CharField(validators = [phoneNumberRegex], max_length = 11, unique = True)
+    GENDER_CHOICES = (
+        (0, 'Female'),
+        (1, 'Male')
+    )
+    gender = models.SmallIntegerField(choices = GENDER_CHOICES)
+    email = models.EmailField(max_length=128)
+    address = models.CharField(max_length=200)
+    point = models.IntegerField(blank=True)
 
-# Create your models here.
+    # Profile
+    # nickname = models.CharField(max_length=100)
+    # intorduction = models.TextField(blank=True)
+    # image
