5ff5e13f60ddad77165bafd71bb210fdcf686c76
Bin0917 <leesd132@naver.com>
2022-07-06 00:26:17 +0900

    승빈2

--- Binbin/main.js
+++ Binbin/main.js
@@ -52,3 +52,41 @@ $(".Login-Content").keypress(function(e){
 		Login();
 	}
 });
+
+//start 페이지 버튼 호버 이미지 변경. 랜덤 수는 데이터베이스에 있는 갯수만큼 해도 될거같슴드아
+var img1 = document.getElementById('img-1');
+var img2 = document.getElementById('img-2');
+var img3 = document.getElementById('img-3');
+
+// var btn = document.querySelector('.button');
+// btn.addEventListener("mouseover",function(){
+// 	var num = Math.floor(Math.random() * 3) + 1;
+// 	img1.src = num+".jpg"
+// 	var num = Math.floor(Math.random() * 3) + 1;
+// 	img2.src = num+".jpg"
+// 	var num = Math.floor(Math.random() * 3) + 1;
+// 	img3.src = num+".jpg"
+// });
+function img_change(){
+	var num = Math.floor(Math.random() * 3) + 1;
+	// fade(img1)
+	img1.src = num+".jpg"
+	var num = Math.floor(Math.random() * 3) + 1;
+	// fade(img2)
+	img2.src = num+".jpg"
+	var num = Math.floor(Math.random() * 3) + 1;
+	// fade(img3)
+	img3.src = num+".jpg"
+}
+// start 이미지 변경 애니메이션
+// function fade(element) {
+//     var op = 0.8;  // initial opacity
+//     var timer = setInterval(function () {
+//         if (op >= 1){
+//             clearInterval(timer);
+//         }
+//         element.style.opacity = op;
+//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op += 0.05;
+//     }, 50);
+// }
