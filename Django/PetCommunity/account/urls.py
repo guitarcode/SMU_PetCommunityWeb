05625216cc36e28910ed9c6a76fa61a1af44d888
Baejb <bjb6478@naver.com>
2022-07-06 23:26:12 +0900

    아이디 찾기 페이지 , 비밀번호 찾기 페이지 , 로그인 버튼 장고 적용 완료

--- Django/PetCommunity/account/urls.py
+++ Django/PetCommunity/account/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.showstart),
     path('signup/', views.singup),
+    path('login/', views.login),
+    path('find/', views.find),
+    path('findId/', views.findId),
 ]
