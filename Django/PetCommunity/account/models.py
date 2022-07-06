dc108c10715373c198a8aab87bd5e9cd8143bdb2
Semin Choi <choicco89@gmail.com>
2022-07-06 23:10:53 +0900

    createsuperuser

--- Django/PetCommunity/account/models.py
+++ Django/PetCommunity/account/models.py
@@ -8,19 +8,20 @@ from django.conf import settings
 
 class Member(AbstractUser):
     #회원id : Model에서 자동으로 생성
-    password = models.CharField(max_length=100)
-    name = models.CharField(max_length=30)
+    id = models.CharField(max_length=30, primary_key = True)
+    password = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null = True)
     # 정규식으로 유효성 검증
     phoneNumberRegex = RegexValidator(regex = r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
-    phoneNumber = models.CharField(validators = [phoneNumberRegex], max_length = 11, unique = True)
+    phoneNumber = models.CharField(validators = [phoneNumberRegex], max_length = 11, unique = True, null = True)
     GENDER_CHOICES = (
         (0, 'Female'),
         (1, 'Male')
     )
-    gender = models.SmallIntegerField(choices = GENDER_CHOICES)
-    email = models.EmailField(max_length=128)
-    address = models.CharField(max_length=200)
-    point = models.IntegerField(blank=True)
+    gender = models.SmallIntegerField(choices = GENDER_CHOICES, null = True)
+    email = models.EmailField(max_length=128, null = True)
+    address = models.CharField(max_length=200, null = True)
+    point = models.IntegerField(blank=True, null = True)
     #이모티콘 외래키 참조
     # emoticon = models.ForeignKey(Emoticon, blank=True, null=True, on_delete=models.SET_NULL)
     #팔로우, 팔로워
